4ac9970e665a374e270be427a7d97da731a58404
Katlego Maphango <mphangokatlego08@gmail.com>
2023-04-13 15:04:01 +0200

    refracted add and multiply functions and changed logic of internal()

--- IWA-14-3_Challenge2/scripts.js
+++ IWA-14-3_Challenge2/scripts.js
@@ -1,10 +1,14 @@
-function add = a, b => { a + b }
-function multiply = a, b => { a - b }
+const add = (a, b) => a + b 
+
+const multiply = (a, b) => a * b
 
 function internal() {
-	const added = this.add(this.a, this.b)
-	this.multiply(this.a, this.b)
-	return this
+	const added = this.add(this.internal.a, this.internal.b)
+	const multi = this.multiply(this.internal.a, this.internal.b)
+	// return `${added} , ${multi}`
+	// this.internal.c = multi - added
+	// return this.internal.c
+	return added*multi
 }
 
 // Not allowed to change below this
@@ -15,21 +19,22 @@ const example1 = {
 		b: 4,
 		c: 8,
 	},
-	add,
-	multiply,
-  calculate: internal
+	add, // a+b=2+4=6
+	multiply, // a*b=2*4=8
+	calculate: internal
 }
 
 const example2 = {
 	internal: {
 		a: 2,
-		b: 4,
+		b: 2,
 		c: 8,
 	},
 	add,
 	multiply,
-  calculate: internal
+	calculate: internal
 }
 
-example1.calculate()
-example2.calculate()
+console.log(example1.calculate()) // 48
+console.log(example2.calculate())  // 12
+// console.log(example1.add)
