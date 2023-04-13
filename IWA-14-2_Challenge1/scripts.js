32314ae514e09e22463138463a4be6aa70a63cf0
Katlego Maphango <mphangokatlego08@gmail.com>
2023-04-13 14:13:28 +0200

    Passed parameter to function and changed function name

--- IWA-14-2_Challenge1/scripts.js
+++ IWA-14-2_Challenge1/scripts.js
@@ -2,13 +2,13 @@ firstName = 'John';
 age = 35;
 hobby = 'Coding';
 
-const logTwice = () => {
-  console(parameter)
-  console(parameter)
+const logTwice = (parameter) => {
+  console.log(parameter)
+  console.log(parameter)
 }
 
-function hobby () {
-  logTwice(`Hello, ${name} (${age}). I love ${hobby}!`)
+function hobbyLog () {
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
 }
 
-hobby()
+hobbyLog()
